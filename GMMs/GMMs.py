6e807e9e2696bcf311db79cfa170ff1f2b5c0088
Todd <todd_stan@163.com>
2021-06-03 09:22:02 +0800

    add bic

--- GMMs/GMMs.py
+++ GMMs/GMMs.py
@@ -1,3 +1,5 @@
+import gif
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from BasicTools import plot_tools
@@ -19,10 +21,10 @@ class GMMs(object):
         self.lh_theta = lh_theta
         self.max_iter = max_iter
 
-        self.iter_record = {'mu_all': [],
-                            'sigma_all': [],
-                            'pi_all': [],
-                            'lh': []}
+        #
+        self.gmms_params = [None, None, None]  # mu_all, sigma_all, pi_all
+        self.train_record = {'gmms_params': [], 'lh': []}
+        self.EPS = 1e-20  # small value to avoid #overflow
 
     def _gm_pdf(self, x, mu, sigma):
         """ the probability density of given data
@@ -69,7 +71,7 @@ class GMMs(object):
                                                    sigma_all[model_i, :, :])
         return p
 
-    def _init_params(self, init_method):
+    def _init_gmms_params(self, init_method):
         """"""
         x = self.x
         k = self.k
@@ -84,43 +86,31 @@ class GMMs(object):
             mu_all[:, axis] = np.linspace(max_ax, min_ax, k)
         elif init_method == 'random':
             mu_all = x[np.random.choice(n_sample, size=k, replace=False), :]
-
         # sigma
         sigma_all = np.zeros((k, n_var, n_var), dtype=np.float32)
         mean_var = np.mean(np.var(x, axis=0))
         for model_i in range(k):
             sigma_all[model_i] = np.eye(n_var)*mean_var/(10*n_var)
-
         # pi
         pi_all = np.ones(k, dtype=np.float32)*1.0/k
 
-        return [mu_all, sigma_all, pi_all]
-
-    def _update_record(self, mu_all, sigma_all, pi_all, lh):
-        self.iter_record['mu_all'].append(mu_all.copy())
-        self.iter_record['sigma_all'].append(sigma_all.copy())
-        self.iter_record['pi_all'].append(pi_all.copy())
-        self.iter_record['lh'].append(lh)
-
-    def _clear_record(self):
-        self.iter_record['mu_all'].clear()
-        self.iter_record['sigma_all'].clear()
-        self.iter_record['pi_all'].clear()
-        self.iter_record['lh'].clear()
+        gmms_params = [mu_all, sigma_all, pi_all]
+        return gmms_params
 
     def _is_stop(self):
         """ return true if likelihood change between two successive iteration
          do not exceed self.lh_theta
         """
-        if (len(self.iter_record['lh']) > 1 and
-            np.abs(self.iter_record['lh'][-1]
-                   - self.iter_record['lh'][-2]) < self.lh_theta):
+        if (len(self.train_record['lh']) > 1 and
+            np.abs(self.train_record['lh'][-1]
+                   - self.train_record['lh'][-2]) < self.lh_theta):
             return True
         else:
             return False
 
-    def EM(self, init_method='random', is_log=False, is_plot=False,
-           fig_path=None, is_gif=False, gif_path=None, fps=10):
+    def EM(self, init_method='random', constrain_sigma_diag=True,
+           is_plot=False, fig_dir=None,
+           is_log=False):
         """Expecation-Maximization algorithm
         Args:
             init_method: method of initialing the center of Gaussian models,
@@ -130,22 +120,22 @@ class GMMs(object):
             n_iter_max: the maximum of iteration
             is_plot: whether plot the training result
             fig_path: file path where image to be saved
-            is_gif: whether to plot the iteration process in gif
-            gif_path: file path where gif to be saved
         """
-        epsilon = 1e-20  # small value to avoid #overflow
 
         k = self.k
         x = self.x
         [n_sample, n_var] = x.shape
 
-        [mu_all, sigma_all, pi_all] = self._init_params(init_method)
+        [mu_all, sigma_all, pi_all] = self._init_gmms_params(init_method)
         lh = np.mean(
                 np.log(
                     self._gmm_pdf(x, mu_all, sigma_all, pi_all)
-                    + epsilon))
-        self._clear_record()
-        self._update_record(mu_all, sigma_all, pi_all, lh)
+                    + self.EPS))
+
+        #
+        self.train_record['gmms_params'] =\
+            [[mu_all.copy(), sigma_all.copy(), pi_all.copy()]]
+        self.train_record['lh'] = [lh]
 
         # condition probability of data, intialized as 0
         posterior = np.zeros((n_sample, k), dtype=np.float32)
@@ -158,12 +148,11 @@ class GMMs(object):
                 for model_i in range(k):
                     posterior[:, model_i] = self._gm_pdf(x, mu_all[model_i],
                                                          sigma_all[model_i])
-
                 # M step
                 posterior = np.divide(
                                 posterior,
                                 (np.sum(posterior, axis=1)[:, np.newaxis]
-                                 + epsilon))
+                                 + self.EPS))
                 for model_i in range(k):
                     if np.isnan(np.max(posterior[:, model_i])):
                         raise ValueError('probability density of data is nan')
@@ -173,17 +162,21 @@ class GMMs(object):
                     mu_all[model_i, :] = (np.dot(x.T, posterior[:, model_i])
                                           / pd_data_model_i)
                     diff_array = x - mu_all[model_i, :]
-                    sigma_all[model_i] = ((np.dot(
-                                            np.multiply(diff_array.T,
-                                                        posterior[:, model_i]),
-                                            diff_array)
-                                           / pd_data_model_i)
-                                          + np.eye(n_var)*epsilon)
+                    sigma_all[model_i] =\
+                        (np.dot((diff_array.T*posterior[:, model_i]),
+                                diff_array)
+                         / pd_data_model_i)
+                    if constrain_sigma_diag:
+                        sigma_all[model_i] = \
+                            sigma_all[model_i]+np.eye(n_var)*self.EPS
 
                 lh = np.mean(
                         np.log(self._gmm_pdf(x, mu_all, sigma_all, pi_all)
-                               + epsilon))
-                self._update_record(mu_all, sigma_all, pi_all, lh)
+                               + self.EPS))
+
+                self.train_record['gmms_params'].append(
+                    [mu_all.copy(), sigma_all.copy(), pi_all.copy()])
+                self.train_record['lh'].append(lh)
 
                 n_iter = n_iter+1
                 if self._is_stop():
@@ -191,6 +184,11 @@ class GMMs(object):
 
                 if is_log:
                     print('iter {} lh {}'.format(iter, lh))
+                if is_plot:
+                    fig, ax = self.visualize(
+                        gmms_params=[mu_all, sigma_all, pi_all])
+                    fig.savefig(f'{fig_dir}/step-{iter}.png')
+                    plt.close()
 
             except Exception as e:
                 except_info = e
@@ -198,144 +196,125 @@ class GMMs(object):
                 n_iter = n_iter-1
                 break
 
-        self.mu_all = mu_all
-        self.sigma_all = sigma_all
-        self.pi_all = pi_all
-
-        if is_gif:
-            gif_writer = plot_tools.Gif()
-            fig, ax = plt.subplots(1, 1)
-            for iter in range(n_iter+1):
-                gif_writer.add(
-                    self.plot(ax, x,
-                              self.iter_record['mu_all'][iter],
-                              self.iter_record['sigma_all'][iter],
-                              self.iter_record['pi_all'][iter]))
-            gif_writer.save(gif_path, fig, fps=fps)
-
-        if is_plot:
-            self.plot_record(fig_path)
+        self.gmms_params = [mu_all, sigma_all, pi_all]
 
         if is_except:
             raise Exception(except_info)
 
-    def plot_record(self, fig_path):
+    def plot_train_gif(self, gif_path, duration=3.5):
+
+        @gif.frame
+        def plot_gif_frame(gmms_params):
+            self.visualize(gmms_params=gmms_params)
+
+        frames = []
+        for gmms_params in self.train_record['gmms_params']:
+            frame = plot_gif_frame(gmms_params=gmms_params)
+            frames.append(frame)
+        gif.save(
+            frames, gif_path, duration=duration, unit='s', between='startend')
+
+    def plot_train_curve(self, fig_path):
         x = self.x
-        fig = plt.figure(figsize=(8, 3))
+        figsize = plot_tools.get_figsize(1, 3)
+        fig, ax = plt.subplots(1, 3, figsize=figsize)
+
         # Befor EM updating
-        ax1 = plt.subplot2grid((2, 3), (0, 0), rowspan=2)
-        self.plot(ax1, x, self.iter_record['mu_all'][0],
-                  self.iter_record['sigma_all'][0],
-                  self.iter_record['pi_all'][0])
-        ax1.set_title('Before EM')
+        self.visualize(
+            ax=ax[0], x=x, gmms_params=self.train_record['gmms_params'][0])
+        ax[0].set_title('Before EM')
 
         # After EM updating
-        ax2 = plt.subplot2grid((2, 3), (0, 1), rowspan=2)
-        self.plot(ax2, x, self.iter_record['mu_all'][-1],
-                  self.iter_record['sigma_all'][-1],
-                  self.iter_record['pi_all'][-1])
-        ax2.set_title('After EM')
+        self.visualize(
+            ax=ax[1], x=x, gmms_params=self.train_record['gmms_params'][-1])
+        ax[1].set_title('After EM')
 
-        # the function of lh about iteration
-        ax3_1 = plt.subplot2grid((2, 3), (0, 2))
-        ax3_1.plot(self.iter_record['lh'])
-        ax3_2 = plt.subplot2grid((2, 3), (1, 2), sharex=ax3_1)
-        ax3_2.plot(self.iter_record['lh'])
+        ax[2].plot(self.train_record['lh'])
+        bic = self.cal_bic()
+        ax[2].text(
+            0.6, 0.2, f'bic: {bic:.2f}', transform=ax[2].transAxes,
+            multialignment='center', fontsize=8,
+            bbox=dict(boxstyle='round', facecolor='wheat', alpha=0.5))
 
-        lh_min = np.min(self.iter_record['lh'][1:])
-        lh_max = np.max(self.iter_record['lh'][1:])
-        ax3_1.set_ylim((lh_min, lh_max+(lh_max-lh_min)/10))
-        ax3_2.set_ylim((self.iter_record['lh'][0]-(lh_max-lh_min)/10,
-                        self.iter_record['lh'][0]+(lh_max-lh_min)/10))
-
-        ax3_1.spines['bottom'].set_visible(False)
-        ax3_2.spines['top'].set_visible(False)
-        ax3_1.xaxis.tick_top()
-        ax3_2.tick_params(labeltop=False)  # don't put tick labels at the top
-        ax3_2.xaxis.tick_bottom()
-        ax3_2.set_xlabel('iteration')
-        ax3_1.set_title('likelihood')
-
-        d = .015  # how big to make the diagonal lines in axes coordinates
-        # arguments to pass to plot, just so we don't keep repeating them
-        kwargs = dict(transform=ax3_1.transAxes, color='k', clip_on=False)
-        ax3_1.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
-        ax3_1.plot((1 - d, 1 + d), (-d, +d), **kwargs)  # top-right diagonal
-
-        kwargs.update(transform=ax3_2.transAxes)  # switch to the bottom axes
-        ax3_2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
-        ax3_2.plot((1 - d, 1 + d), (1 - d, 1 + d),
-                   **kwargs)  # bottom-right diagonal
-
-        plt.tight_layout()
         if fig_path is not None:
             fig.savefig(fig_path)
         plt.close(fig)
 
-    def cal_prob(self, x):
-        """
-        """
-        return self._gmm_pdf(x, self.mu_all, self.sigma_all, self.pi_all)
-
-    def plot(self, ax, x, mu_all, sigma_all, pi_all):
+    def visualize(self, x=None, gmms_params=None, ax=None, fig=None):
         """ GMMs(contours) as well as data(scatter)
         Args:
-            ax: axis
-            x: numpy ndarray with shape of [n_sample, n_var]
-            mu_all, sigma_all: parameters of each GM component
-            pi: weights of each GM component
+            x: numpy ndarray with shape of [n_sample, n_var]. if not given,
+                data for GMMs settings will be used
+            gmms_prams: [mu_all, sigma_all, pi_all]. if not given,
+                current GMMs params will be used
         Returns:
-            list of line objects
+            figure
         """
-        line_all = []
+        if x is None:
+            x = self.x
 
-        # k = mu_all.shape[0]
+        if gmms_params is None:
+            gmms_params = self.gmms_params
+        mu_all, sigma_all, pi_all = gmms_params
+
+        if ax is None:
+            fig, ax = plt.subplots(1, 1, constrained_layout=True)
 
         plot_tools.plot_line(ax, x[:, 0], x[:, 1], 'bo', alpha=0.3, zorder=1,
-                             markersize=3, line_container=line_all)
-
+                             markersize=3)
+        # make grid within specified region
         n_point = 1000
-
         x0, x1 = np.meshgrid(
-                    np.linspace(np.min(x[:, 0]), np.max(x[:, 0]), n_point),
-                    np.linspace(np.min(x[:, 1]), np.max(x[:, 1]), n_point))
+            np.linspace(np.min(x[:, 0]), np.max(x[:, 0]), n_point),
+            np.linspace(np.min(x[:, 1]), np.max(x[:, 1]), n_point))
         x0x1 = np.array([x0.flatten(), x1.flatten()]).T
+        # cal pdf of each point on grid
         p = self._gmm_pdf(x0x1, mu_all, sigma_all, pi_all)
-
-        # levels = np.logspace(np.log(np.min(p)), np.log(np.max(p)), 100)
+        # divide p range in to 100 segments
         levels = np.linspace(np.min(p), np.max(p), 100)
-        index_round = np.argmin(np.abs(p[:, np.newaxis]-levels[np.newaxis, :]),
-                                axis=1)
+        # assigment each data to corresponding segments
+        index_round = np.argmin(
+            np.abs(p[:, np.newaxis]-levels[np.newaxis, :]), axis=1)
         for i in range(100):
             p[index_round == i] = levels[i]
 
         p = p.reshape([x0.shape[0], x1.shape[0]])
-        plot_tools.plot_contour(ax, x0, x1, p,
-                                levels=[levels[i] for i in range(0, 100, 20)],
-                                alpha=1, zorder=2,
-                                line_container=line_all, colors='k')
+        plot_tools.plot_contour(
+            ax, x0, x1, p,
+            levels=[levels[i] for i in range(0, 100, 20)],
+            alpha=1, zorder=2, colors='k')
         plot_tools.plot_line(ax, mu_all[:, 0], mu_all[:, 1], 'ro',
-                             markersize=3, line_container=line_all,
-                             zorder=3)
-        return line_all
+                             markersize=3, zorder=3)
+        return fig, ax
 
-    def save(self, model_path):
-        np.savez(model_path,
-                 mu_all=self.mu_all,
-                 sigma_all=self.sigma_all,
-                 pi_all=self.pi_all,
-                 iter_record=self.iter_record,
-                 lh_theta=self.lh_theta,
-                 max_iter=self.max_iter)
+    def save(self, gmms_params_path, train_record_path=None):
+        with open(gmms_params_path, 'wb') as gmms_params_f:
+            pickle.dump(self.gmms_params, gmms_params_f)
 
-    def load(self, model_path):
-        data_pack = np.load(model_path, allow_pickle=True)
-        self.mu_all = data_pack['mu_all']
-        self.sigma_all = data_pack['sigma_all']
-        self.pi_all = data_pack['pi_all']
-        self.iter_record = data_pack['iter_record']
-        self.lh_theta = data_pack['lh_theta']
-        self.max_iter = data_pack['max_iter']
+        if train_record_path is not None:
+            with open(train_record_path, 'wb') as train_record_f:
+                pickle.dump(self.train_record, train_record_f)
+
+    def load(self, gmms_params_path, train_record_path=None):
+        with open(gmms_params_path, 'rb') as gmms_params_f:
+            self.gmms_params = pickle.load(gmms_params_f)
+
+        if train_record_path is not None:
+            with open(train_record_path, 'rb') as train_record_f:
+                self.train_record = pickle.load(train_record_f)
+
+    def cal_prob(self, x):
+        mu_all, sigma_all, pi_all = self.gmms_params
+        return self._gmm_pdf(x, mu_all, sigma_all, pi_all)
+
+    def cal_bic(self):
+        """ Bayesian information criterion
+        """
+        n_sample, n_var = self.x.shape
+        n_params = self.k*n_var + self.k**2 + self.k  # mean+sigma+pi
+        bic = (n_params*np.log(n_sample)
+               - 2*np.sum(np.log(self.cal_prob(self.x)+self.EPS)))
+        return bic
 
 
 class GMMs_classifier:
--- example/example.py
+++ example/example.py
@@ -1,6 +1,10 @@
+import os
+import pickle
 import numpy as np
-from BasicTools import plot_tools
 from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+
+from BasicTools import plot_tools
 from GMMs import GMMs, k_means
 
 
@@ -14,14 +18,44 @@ def k_means_test():
     plot_tools.savefig(fig)
 
 
-def GMMs_test():
-    X, Y = make_blobs(cluster_std=1, random_state=np.random.randint(100),
-                      n_samples=800, centers=3)
+def gen_random_dataset(n_center, n_sample):
+    dataset_path = f'dataset-center_{n_center}-sample_{n_sample}.pkl'
+    random_state = np.random.RandomState()
+    if not os.path.exists(dataset_path):
+        X, Y = make_blobs(
+            cluster_std=random_state.rand()*10,
+            random_state=random_state,
+            n_samples=n_sample, centers=3)
+        with open(dataset_path, 'wb') as dataset_f:
+            pickle.dump([X, Y], dataset_f)
+    else:
+        with open(dataset_path, 'rb') as dataset_f:
+            X, Y = pickle.load(dataset_f)
+    return X, Y
 
-    gmms = GMMs(x=X, k=3, max_iter=100, lh_theta=1e-5)
-    gmms.EM(init_method='random',
-            is_plot=True, fig_fpath='images/example.png',
-            is_gif=True, gif_fpath='images/example.gif')
+
+def GMMs_test():
+
+    X, Y = gen_random_dataset(4, 4000)
+
+    k_all = np.arange(2, 12, 2)
+    n_k = k_all.shape[0]
+    pic_record = np.arange(n_k)
+    for i, k in enumerate(k_all):
+        gmms = GMMs(x=X, k=k, max_iter=2000, lh_theta=1e-10)
+        gmms.EM(init_method='random')
+        bic = gmms.cal_bic()
+        pic_record[i] = bic
+        gmms.plot_train_curve(f'train_record-{k}.png')
+        fig, ax = gmms.visualize()
+        ax.set_title(f'{k}: {bic:.2f}')
+        fig.savefig(f'visualize-{k}.png')
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(k_all, pic_record)
+    ax.set_xlabel('k')
+    ax.set_ylabel('PIC')
+    fig.savefig('pic_k_func.png')
 
 
 if __name__ == '__main__':
